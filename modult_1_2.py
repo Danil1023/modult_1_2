05166c4007469bd66371bb96f2d55a82036ce50a
Danil1023 <tolstobokovdanil1903@gmail.com>
2024-08-19 23:25:35 +1000

    Update modult_1_2.py

--- modult_1_2.py
+++ modult_1_2.py
@@ -1,5 +1,6 @@
 completed_homework = 12
 hours_spent = 1.5
+print(12 / 1.5)
 course_title = "Python"
 time_for_one_task = 8
 print('Курс', course_title, 'всего задач:', completed_homework, 'затрачено часов:', hours_spent, 'среднее время выполнения:', time_for_one_task, 'минут')
